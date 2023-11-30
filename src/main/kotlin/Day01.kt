f29669f2acf2fd8a943f5ac8be0303e3b082a4ee
Jakub Tobolski <jakub.tobolski@allegrogroup.com>
2023-11-30 23:04:23 +0100

    template

---
+++ src/main/kotlin/Day01.kt
@@ -0,0 +1,13 @@
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readInput("Day01")
+    println(testInput)
+}
---
+++ src/main/kotlin/Utils.kt
@@ -0,0 +1,31 @@
+import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+fun readInput(name: String) = Path("src/main/data/$name").readLines()
+
+fun dailyTemplate(fileName: String) {
+    val content = """
+        fun main() {
+            fun part1(input: List<String>): Int {
+                return input.size
+            }
+
+            fun part2(input: List<String>): Int {
+                return input.size
+            }
+
+            val testInput = readInput("$fileName")
+            println(testInput)
+        }
+    """.trimIndent()
+
+    val codeFile = "src/main/kotlin/$fileName.kt"
+    val dataFile = "src/main/data/$fileName"
+    File(codeFile).writeText(content)
+    File(dataFile).createNewFile()
+}
+
+fun main() {
+    dailyTemplate("Day02")
+}
