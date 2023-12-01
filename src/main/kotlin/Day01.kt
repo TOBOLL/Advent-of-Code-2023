10b3b000f679e1bea89008b6ff86244fb01ee6d2
Jakub Tobolski <jakub.tobolski@allegrogroup.com>
2023-12-01 21:32:05 +0100

    Day 01 - better solution

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,45 +1,38 @@
-import java.lang.RuntimeException
-
 fun main() {
-    fun part1(input: List<String>): Int {
-        val calibrationValues = input.map { line -> line.filter { it.isDigit() } }.map { "${it.first()}${it.last()}" }
-        return calibrationValues.sumOf { it.toInt() }
-    }
-
-    val numberMap = mapOf(
-        "one" to "1",
-        "two" to "2",
-        "three" to "3",
-        "four" to "4",
-        "five" to "5",
-        "six" to "6",
-        "seven" to "7",
-        "eight" to "8",
-        "nine" to "9",
-    )
-
-    fun findDigit(line: String, revers: Boolean = false): String {
-        val indices = if (revers) line.length - 1 downTo 0 else line.indices
-
-        return indices
-            .asSequence()
-            .map { index ->
-                when {
-                    line[index].isDigit() -> line[index].toString()
-                    else -> numberMap[line.substring(index)]
-                }
-            }
-            .firstOrNull { it != null } ?: throw RuntimeException("There is no digits")
-    }
-
-    fun part2(input: List<String>): Int {
-        return input
-            .asSequence()
-            .map { findDigit(it) + findDigit(it, true) }
-            .sumOf { it.toInt() }
-    }
-
     val input = readInput("Day01")
     println("Part one: ${part1(input)}")
     println("Part two: ${part2(input)}")
 }
+
+fun part1(input: List<String>): Int {
+    return input.sumOf { line ->
+        line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
+    }
+}
+
+fun part2(input: List<String>): Int {
+    return input.sumOf { line -> line.findAnyOf(numberMap.keys).getValue() * 10 + line.findLastAnyOf(numberMap.keys).getValue() }
+}
+
+fun Pair<Int, String>?.getValue() = numberMap.getValue(checkNotNull(this).second).toInt()
+
+val numberMap = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+    "1" to 1,
+    "2" to 2,
+    "3" to 3,
+    "4" to 4,
+    "5" to 5,
+    "6" to 6,
+    "7" to 7,
+    "8" to 8,
+    "9" to 9,
+)
